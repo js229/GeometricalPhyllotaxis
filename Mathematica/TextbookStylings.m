d4eee5eab39573094554865bec9767021096ec06
js229 <jonathan@swintons.net>
2021-02-07 19:34:36 +0000

    Update TextbookStylings.m

--- Mathematica/TextbookStylings.m
+++ Mathematica/TextbookStylings.m
@@ -16,8 +16,8 @@ jParastichyColour = <| 1 -> Red, 2 -> Blue, 3 -> Green,
 SetAttributes[jExpo,HoldFirst];
 jExpo[fig_] := Module[{figname},
 figname=SymbolName[Unevaluated[fig]];
-Export[StringJoin[(*"mma-",*)figname,".jpg"],fig,ImageResolution->600];
-Export[StringJoin[(*"mma-",*)figname,".pdf"],fig,ImageResolution->600];
+Export[StringJoin[figname,".jpg"],fig,ImageResolution->600];
+Export[StringJoin[figname,".pdf"],fig,ImageResolution->600];
 fig
 ];
 
@@ -35,14 +35,5 @@ jStyle = Association[
 End[]
 
 EndPackage[]
-
-(*SetOptions[ParametricPlot,AxesStyle->Directive[FontFamily->fontFamily],
-BaseStyle->Directive[fontFamily],
-LabelStyle->Directive[fontFamily]];
-SetOptions[Plot,AxesStyle->Directive[Red,FontFamily->fontFamily],
-BaseStyle->Directive[Blue,fontFamily],
-LabelStyle->Directive[Blue,fontFamily]];
-*)
-
 
 
